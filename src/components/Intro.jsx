a2c112962f268eb68b0e16b0fb400564e68305c7
Gabriela Torrini <gabriela.torrini@outlook.com>
2023-10-19 10:13:55 -0500

    New component to introduce game

---
+++ src/components/Intro.jsx
@@ -0,0 +1,10 @@
+import { React } from 'react';
+
+export default function Intro() {
+    return (
+        <>
+            <h3>Welcome to Astro Escape!</h3>
+            <p>Your spaceship has just entered the sphere of influence of a black hole. Can you escape? </p>
+        </>
+    )
+}
