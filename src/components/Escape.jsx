ba48653bb8071092e2e871b0588ecfb423677dee
Gabriela Torrini <gabriela.torrini@outlook.com>
2023-10-19 15:57:36 -0500

    Added theme overrides

--- src/components/Escape.jsx
+++ src/components/Escape.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
-import { Box, Button, Grid, Step, Stepper, StepLabel } from '@mui/material';
-import { BackButton, NextButton } from './NavButtons.jsx';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Box, Step, Stepper, StepLabel } from '@mui/material';
 import Intro from './Intro.jsx';
-// import Clue1 from './CommsPanel.jsx';
-// import Clue2 from './CaesarCipher.jsx';
+import Controls from './CommsPanel.jsx';
 
-const steps = ['Intro']
+const astroEscapeTheme = createTheme({
+    components: {
+        MuiStepIcon: {
+            styleOverrides: {
+                text: {
+                    fontFamily: 'Consolas',
+                },
+            },
+        },
+    },
+})
+
+const steps = ['Intro', 'Clue 1']
 
 export default function HorizontalLinearStepper() {
     const [activeStep, setActiveStep] = React.useState(0);
@@ -19,75 +29,37 @@ export default function HorizontalLinearStepper() {
       setActiveStep((prevActiveStep) => prevActiveStep - 1);
     };
   
-    const handleReset = () => {
-      setActiveStep(0);
-    };
-
-    const handleNav = () => {
-        switch(activeStep) {
-            case (0):
-                return (
-                    <Grid item xs={12}>
-                        <NextButton handleClick={handleNext}></NextButton>
-                    </Grid>
-                )
-            case (steps.length):
-                return (
-                    <>
-                        <Grid item xs={12}>
-                            <Button handleClick={handleReset}></Button>
-                        </Grid>
-                    </>
-                )
-            default:
-                return (
-                    <>
-                        <Grid item xs={6}>
-                            <BackButton handleClick={handleBack}></BackButton>
-                        </Grid>
-                        <Grid item xs={6}>
-                            <NextButton handleClick={handleNext}></NextButton>
-                        </Grid>
-                    </>
-                )
-        }
-    }
+    // const handleReset = () => {
+    //   setActiveStep(0);
+    // };
 
     const handleContent = () => {
         switch(activeStep) {
             case(0):
                 return(
-                    <Intro></Intro>
-                )
-            // case(1):
-            //     return(
-            //         <Clue1></Clue1>
-            //     )
-            // case(2):
-            //     return(
-            //         <Clue2></Clue2>
-            //     )
+                    <Intro next={handleNext}></Intro>
+                );
+            case(1):
+                return(
+                    <Controls back={handleBack} next={handleNext}></Controls>
+                );
         }
-    }
+    };
   
     return (
         <>
             <Box sx={{ mx: 2, my: 2}}>
-                <Stepper activeStep={activeStep}>
-                    <Step key={steps[0]}>
-                        <StepLabel>{steps[0]}</StepLabel>
-                    </Step>
-                    {/* <Step key={steps[1]}>
-                        <StepLabel>{steps[1]}</StepLabel>
-                    </Step>
-                    <Step key={steps[2]}>
-                        <StepLabel>{steps[2]}</StepLabel>
-                    </Step> */}
-                </Stepper>
-                {handleContent()}
-                <Grid container>
-                    {handleNav()}
-                </Grid>
+                <ThemeProvider theme={astroEscapeTheme}>
+                    <Stepper activeStep={activeStep}>
+                        <Step key={steps[0]}>
+                            <StepLabel>{steps[0]}</StepLabel>
+                        </Step>
+                            <Step key={steps[1]}>
+                            <StepLabel>{steps[1]}</StepLabel>
+                        </Step>
+                    </Stepper>
+                    {handleContent()}
+                </ThemeProvider>
             </Box>
         </>
     );
