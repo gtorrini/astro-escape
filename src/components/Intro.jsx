fe19c5de7b25a0a3ad3f8e756526d57154939147
Gabriela Torrini <gabriela.torrini@outlook.com>
2023-10-19 15:58:07 -0500

    Added grid for nav arrows and specified propTypes

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,10 +1,22 @@
 import { React } from 'react';
+import PropTypes from 'prop-types';
+import Grid from '@mui/material/Grid';
+import { NextButton } from './NavButtons.jsx';
 
-export default function Intro() {
+export default function Intro(props) {
     return (
         <>
             <h3>Welcome to Astro Escape!</h3>
             <p>Your spaceship has just entered the sphere of influence of a black hole. Can you escape? </p>
+            <Grid container>
+                <Grid item xs={12}>
+                    <NextButton handleClick={props.next}></NextButton>
+                </Grid>
+            </Grid>
         </>
-    )
+    );
 }
+
+Intro.propTypes = {
+    next: PropTypes.func.isRequired
+};
