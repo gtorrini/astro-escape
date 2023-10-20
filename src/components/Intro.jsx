ec684d5f795666d1ba9c58aef45542cdfd1c6634
Gabriela Torrini <gabriela.torrini@outlook.com>
2023-10-20 17:08:34 -0500

    Added MUI typography

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,13 +1,22 @@
 import { React } from 'react';
 import PropTypes from 'prop-types';
-import Grid from '@mui/material/Grid';
+import { Box, Grid, Typography } from '@mui/material';
 import { NextButton } from './NavButtons.jsx';
 
 export default function Intro(props) {
     return (
         <>
-            <h3>Welcome to Astro Escape!</h3>
-            <p>Your spaceship has just entered the sphere of influence of a black hole. Can you escape? </p>
+            <Box sx={{my: 5}}>
+                <Typography variant="h4">
+                    Welcome to Astro Escape
+                </Typography>
+                <Typography variant="subtitle1" sx={{mb: 3}}>
+                    An Astronomy-Themed Online Escape Room
+                </Typography>
+                <Typography variant="body1">
+                    Your spaceship has just entered the sphere of influence of a black hole. Can you escape?
+                </Typography>
+            </Box>
             <Grid container>
                 <Grid item xs={12}>
                     <NextButton handleClick={props.next}></NextButton>
