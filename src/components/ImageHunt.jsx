60969bc5a9d8f9817c926583792c9c72c8625402
Gabriela Torrini <gabriela.torrini@outlook.com>
2023-10-27 14:17:47 -0400

    Added func to calc responsive coords for image map

--- src/components/ImageHunt.jsx
+++ src/components/ImageHunt.jsx
@@ -11,6 +11,30 @@ export default function ImageHunt(props) {
         setFound(true);
     }
 
+    const calcCoords = () => {
+        const width = window.innerWidth;
+
+        if (width <= 480) {
+            return "75, 85, 110, 90, 105, 130, 65, 120";
+        }
+        // Coords for tablets
+        else if (width > 480 && width <= 768) {
+            return "110, 120, 155, 130, 145, 180, 100, 170";
+        }
+        // Coords for small laptops
+        else if (width > 768 && width <= 1024) {
+            return "170, 185, 240, 195, 220, 280, 145, 260";
+        }
+        // Coords for large laptops
+        else if (width > 1024 && width <= 1280) {
+            return "260, 280, 360, 295, 335, 420, 225, 400";
+        }
+        // Coords for desktops
+        else if (width > 1280 && width <= 1600) {
+            return "320, 345, 450, 365, 425, 525, 280, 500";
+        }
+    }
+
     return (
         <>
             <Box sx={{my: 5}}>
@@ -22,11 +46,11 @@ export default function ImageHunt(props) {
                 </Typography>
             </Box>
             <Box sx={{my: 5}}>
-                <img src={ppk} alt="Personal items" useMap="#ppk" width="898" height="561"></img>
+                <img src={ppk} id="img-search" alt="Personal items" useMap="#ppk"></img>
                 <map name="ppk">
                     <area 
                         shape="poly"
-                        coords="260, 280, 360, 295, 335, 420, 225, 400"
+                        coords={calcCoords()}
                         alt="Manual"
                         href="javascript:void(0);"
                         onClick={() => handleFound()}
