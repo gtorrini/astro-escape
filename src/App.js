1eabf5feaf551d9b9dad6f626808908512253459
Gabriela Torrini <gabriela.torrini@outlook.com>
2023-10-29 16:18:26 -0400

    Added app-wide MUI theme/overrides and router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
 // import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import React from 'react';
-import HorizontalLinearStepper from './components/Escape.jsx';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Intro from './components/Intro.jsx';
+import Escape from './components/Escape.jsx';
+
+const darkTheme = createTheme({
+  palette: {
+      mode: 'dark',
+  },
+  typography: {
+      h4: {
+          fontFamily: "Silkscreen",
+      },
+      h6: {
+          fontFamily: "Silkscreen",
+      },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <HorizontalLinearStepper></HorizontalLinearStepper>
-    </div>
+        <ThemeProvider theme={darkTheme}>
+          <div className="App">
+            <Router>
+              <Routes>
+                <Route path='/' element={<Intro></Intro>}></Route>
+                <Route path='/play' element={<Escape></Escape>}></Route>
+                {/* <Route path='/about'></Route> */}
+              </Routes>
+            </Router>
+          </div>
+        </ThemeProvider>
   );
 }
 
