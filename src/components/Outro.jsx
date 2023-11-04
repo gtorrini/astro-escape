a8fc5e349a40124982c54320719dde60c89e8e32
Gabriela Torrini <gabriela.torrini@outlook.com>
2023-11-04 18:30:19 -0400

    New component shown upon game completion

---
+++ src/components/Outro.jsx
@@ -0,0 +1,17 @@
+import { React } from 'react';
+import { Box, Typography } from '@mui/material';
+
+export default function Outro() {
+    return (
+        <>
+            <Box sx={{my: 5, maxWidth: '60%', mx: 'auto'}}>
+                <Typography variant="h4">
+                    {'\u2726'} CONGRATULATIONS {'\u2726'}
+                </Typography>
+                <Typography variant="body1" sx={{my: 3}}>
+                    Thanks for playing!
+                </Typography>
+            </Box>
+        </>
+    );
+}
