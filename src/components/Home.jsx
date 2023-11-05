4317b87444631813237be1093ce65537d2171835
Gabriela Torrini <gabriela.torrini@outlook.com>
2023-11-05 15:24:37 -0500

    Renamed component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,7 @@
 import { React } from 'react';
 import { Box, Typography } from '@mui/material';
 
-export default function Intr() {
+export default function Home() {
     return (
         <>
             <Box sx={{my: 5}}>
