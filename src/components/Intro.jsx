d26cebb70180f9269d977643a60adb4a4ddbabc7
Gabriela Torrini <gabriela.torrini@outlook.com>
2023-11-05 14:24:15 -0500

    Modified heading; removed nav buttons

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,18 +1,16 @@
 import { React } from 'react';
-import { Box, Button, Typography } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 
 export default function Intro() {
     return (
         <>
             <Box sx={{my: 5}}>
                 <Typography variant="h4">
-                    ASTRO {'\u2726'} ESCAPE
+                    Welcome!
                 </Typography>
                 <Typography variant="body1" sx={{my: 3}}>
-                    Your spaceship has just entered the sphere of influence of a black hole. Can you escape?
+                    You have just entered space near a black hole. Can you escape?
                 </Typography>
-                <Button href="/play" variant="contained" sx={{mx: 2}}>Play Now</Button>
-                <Button href="/about" variant="contained" sx={{mx: 2}}>About</Button>
             </Box>
         </>
     );
