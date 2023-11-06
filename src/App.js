fadfcf5e345be3a50458429a514bd7db47628fd3
Gabriela Torrini <gabriela.torrini@outlook.com>
2023-11-06 17:14:01 -0500

    Organized imports; disabled MUI ripples; added footer

--- src/App.js
+++ src/App.js
@@ -1,43 +1,66 @@
-import './App.css';
+// 3rd-party imports
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import React from 'react';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
 import { createTheme, responsiveFontSizes, ThemeProvider } from '@mui/material/styles';
-import ResponsiveAppBar from './components/NavBar.jsx';
+ 
+// Local imports
+import './App.css';
+import About from './components/About.jsx';
 import Home from './components/Home.jsx';
 import Escape from './components/Escape.jsx';
-import About from './components/About.jsx';
+import ResponsiveAppBar from './components/NavBar.jsx';
 
+// App-wide MUI styling and overrides
 let darkTheme = createTheme({
   palette: {
       mode: 'dark',
   },
   typography: {
-      h4: {
-          fontFamily: "Silkscreen",
+    h4: {
+        fontFamily: "Silkscreen",
+    },
+    h5: {
+        fontFamily: "Silkscreen",
+    },
+  },
+  components: {
+    MuiButtonBase: {
+      defaultProps: {
+        disableRipple: true, // No button ripples in whole app
       },
-      h5: {
-          fontFamily: "Silkscreen",
+    },
+    MuiButton: {
+      defaultProps: {
+        disableRipple: true, // No button ripples in whole app
       },
+    },
   }
 });
 
 darkTheme = responsiveFontSizes(darkTheme);
 
-function App() {
+export default function App() {
   return (
         <ThemeProvider theme={darkTheme}>
+          <Router>
+            <Routes>
+              <Route path='/' element={<Home></Home>}></Route>
+              <Route path='/play' element={<Escape></Escape>}></Route>
+              <Route path='/about' element={<About></About>}></Route>
+            </Routes>
+          </Router>
           <ResponsiveAppBar></ResponsiveAppBar>
-          <main className="App">
-            <Router>
-              <Routes>
-                <Route path='/' element={<Home></Home>}></Route>
-                <Route path='/play' element={<Escape></Escape>}></Route>
-                <Route path='/about' element={<About></About>}></Route>
-              </Routes>
-            </Router>
+          <main>
           </main>
+          <footer>
+            <Container maxWidth='xl' sx={{ backgroundColor: '#193154' }}>
+              <Typography variant='body1' sx={{ padding: 1, textAlign: 'center'}}>
+                Copyright {'\u00A9'} 2023 Gabriela Torrini
+              </Typography>
+            </Container>
+          </footer>
         </ThemeProvider>
   );
 }
-
-export default App;
