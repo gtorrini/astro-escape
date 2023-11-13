857e67e0dcc8b6861deb2e5b8cd1b632bc69ff3b
Gabriela Torrini <gabriela.torrini@outlook.com>
2023-11-13 17:35:05 -0500

    Created new component for stateless puzzle heading/intro explanation

--- src/components/PuzzleIntros.jsx
+++ src/components/PuzzleIntros.jsx
@@ -3,13 +3,19 @@ import PropTypes from 'prop-types';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 
-function PuzzleIntro(props) {
+export default function PuzzleIntro(props) {
   return (
-    <Box sx={{ my: 5 }}>
+    <Box 
+      sx={{ 
+        maxWidth: (window.innerWidth <= 650) ? '100%' : '60%',
+        mx: 'auto',
+        my: 5 
+      }}
+    >
       <Typography className="site-heading" variant='h5' gutterBottom>
         {props.title}
       </Typography>
-      <Typography variant='body1'>
+      <Typography variant='body1' sx={{ textAlign: 'center' }}>
         {props.blurb}
       </Typography>
     </Box>
