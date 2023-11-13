8f8df05c0042160d8ff3b21d19576e1e97299b8e
Gabriela Torrini <gabriela.torrini@outlook.com>
2023-11-13 13:13:18 -0500

    Individualized imports to improve bundling; removed closing tags on childless components

--- src/components/Escape.jsx
+++ src/components/Escape.jsx
@@ -1,5 +1,11 @@
+// 3rd-party imports 
 import { React, useState } from 'react';
-import { Box, Step, Stepper, StepLabel } from '@mui/material';
+import Box from '@mui/material/Box';
+import Step from '@mui/material/Step';
+import Stepper from '@mui/material/Stepper';
+import StepLabel from '@mui/material/StepLabel';
+
+// Local imports
 import ImageHunt from './ImageHunt.jsx';
 import Protocol from './Protocol.jsx';
 import Controls from './Controls.jsx';
@@ -26,27 +32,27 @@ export default function Escape() {
         switch(activeStep) {
             case(0): 
                 return(
-                    <ImageHunt next={handleNext}></ImageHunt>
+                    <ImageHunt next={handleNext}/>
                 );
             case(1):
                 return(
-                    <Protocol back={handleBack} next={handleNext} restart={handleRestart}></Protocol>
+                    <Protocol back={handleBack} next={handleNext} restart={handleRestart}/>
                 );
             case(2):
                 return(
-                    <Controls back={handleBack} next={handleNext} restart={handleRestart}></Controls>
+                    <Controls back={handleBack} next={handleNext} restart={handleRestart}/>
                 );
             case(3):
                 return(
-                    <Cipher back={handleBack} next={handleNext} restart={handleRestart}></Cipher>
+                    <Cipher back={handleBack} next={handleNext} restart={handleRestart}/>
                 );
             case(4):
                 return(
-                    <Rebus back={handleBack} next={handleNext} restart={handleRestart}></Rebus>
+                    <Rebus back={handleBack} next={handleNext} restart={handleRestart}/>
                 );
             case(5):
                 return(
-                    <Outro></Outro>
+                    <Outro/>
                 );
         }
     };
@@ -56,19 +62,19 @@ export default function Escape() {
             <Box sx={{ mx: 2, my: 2}}>
                 <Stepper activeStep={activeStep}>
                     <Step key="puzzle-1">
-                        <StepLabel></StepLabel>
+                        <StepLabel/>
                     </Step>
                     <Step key="puzzle-2">
-                        <StepLabel></StepLabel>
+                      <StepLabel/>
                     </Step>
                     <Step key="puzzle-3">
-                        <StepLabel></StepLabel>
+                      <StepLabel/>
                     </Step>
                     <Step key="puzzle-4">
-                        <StepLabel></StepLabel>
+                    <StepLabel/>
                     </Step>
                     <Step key="puzzle-5">
-                        <StepLabel></StepLabel>
+                      <StepLabel/>
                     </Step>
                 </Stepper>
                 {handleContent()}
