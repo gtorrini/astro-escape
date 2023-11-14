c61a5fc7ca6cb222d098a5c3fe401b9e203395ca
Gabriela Torrini <gabriela.torrini@outlook.com>
2023-11-14 17:38:16 -0500

    Memoized nav buttons

--- src/components/NavButtons.jsx
+++ src/components/NavButtons.jsx
@@ -1,5 +1,5 @@
 // 3rd party imports
-import React from 'react';
+import {memo, React} from 'react';
 import PropTypes from 'prop-types';
 import Button from '@mui/material/Button'; 
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
@@ -7,77 +7,83 @@ import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import RestartAltIcon from '@mui/icons-material/RestartAlt';
 
 // Access previous puzzle
-function BackButton(props) {
+const BackButton = memo(
+  function BackButton(props) {
     return (
-        <>
-            <Button
-                aria-label="Previous puzzle"
-                onClick={() => props.handleClick()}
-                onKeyDown={(e) => {
-                  if (e.key === "Enter") {
-                    props.handleClick();
-                  }
-                }}
-                variant='contained'
-            >
-                <ArrowBackIcon></ArrowBackIcon>
-            </Button>
-        </>
+      <>
+          <Button
+            aria-label="Previous puzzle"
+            onClick={() => props.handleClick()}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                props.handleClick();
+              }
+            }}
+            variant='contained'
+          >
+            <ArrowBackIcon/>
+          </Button>
+      </>
     );
-}
+  }
+);
 
 BackButton.propTypes = {
     handleClick: PropTypes.func.isRequired
 }
 
 // Access the next puzzle
-function NextButton(props) {
+const NextButton = memo (
+  function NextButton(props) {
     return (
-        <>
-            <Button
-                aria-label="Next puzzle"
-                disabled={props.disabled}
-                onClick={() => props.handleClick()}
-                onKeyDown={(e) => {
-                  if (e.key === "Enter") {
-                    props.handleClick();
-                  }
-                }}
-                variant='contained'
-            >
-                <ArrowForwardIcon></ArrowForwardIcon>
-            </Button>
-        </>
+      <>
+        <Button
+          aria-label="Next puzzle"
+          disabled={props.disabled}
+          onClick={() => props.handleClick()}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              props.handleClick();
+            }
+          }}
+          variant='contained'
+        >
+          <ArrowForwardIcon/>
+        </Button>
+      </>
     );
-}
+  }
+);
 
 NextButton.propTypes = {
-    handleClick: PropTypes.func.isRequired,
-    disabled: PropTypes.bool.isRequired
+  handleClick: PropTypes.func.isRequired,
+  disabled: PropTypes.bool.isRequired
 }
 
 // Reset the game (start from puzzle 1)
-function RestartButton(props) {
+const RestartButton = memo (
+  function RestartButton(props) {
     return (
-        <>
-            <Button
-                aria-label="Restart game"
-                onClick={() => props.handleClick()}
-                onKeyDown={(e) => {
-                  if (e.key === "Enter") {
-                    props.handleClick();
-                  }
-                }}
-                variant='contained'
-            >
-                <RestartAltIcon></RestartAltIcon>
-            </Button>
-        </>
+      <>
+        <Button
+          aria-label="Restart game"
+          onClick={() => props.handleClick()}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              props.handleClick();
+            }
+          }}
+          variant='contained'
+        >
+          <RestartAltIcon/>
+        </Button>
+      </>
     );
-}
+  }
+);
 
 RestartButton.propTypes = {
-    handleClick: PropTypes.func.isRequired
+  handleClick: PropTypes.func.isRequired
 }
 
 export { BackButton, NextButton, RestartButton };
