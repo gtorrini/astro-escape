12241786e377ae502916b17060715cd40a3a0a1d
Gabriela Torrini <gabriela.torrini@outlook.com>
2023-11-16 16:17:29 -0600

    Fixed layout for small screens

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,13 +11,20 @@ export default function About() {
                 <Typography className="site-heading" variant="h4">
                     ABOUT
                 </Typography>
-                <Typography variant="body1" sx={{my: 3, maxWidth: '60%', mx: 'auto'}}>
+                <Typography 
+                  variant="body1" 
+                  sx= {{
+                    my: 3, 
+                    maxWidth: (window.innerWidth < 480) ? '90%' : '60%', 
+                    mx: 'auto'
+                  }}
+                >
                     Astro Escape was developed by Gabriela Torrini, a software engineer and astronomer. She also designed the game&#39;s pixel art background. 
                 </Typography>
                 <Button 
                     href="https://gtorrini.github.io/"
                     variant="contained"
-                    sx={{mx: 1}}
+                    sx={{ mx: 1, mb: (window.innerWidth < 715) ? 1 : 0}}
                 >
                     Visit Personal Site &nbsp;
                     <LanguageIcon></LanguageIcon>
@@ -25,7 +32,7 @@ export default function About() {
                 <Button 
                     href="https://github.com/gtorrini/portfolio"
                     variant="contained"
-                    sx={{mx: 1}}
+                    sx={{mx: 1, mb: (window.innerWidth < 715) ? 1 : 0}}
                 >
                     View Portfolio &nbsp;
                     <GitHubIcon></GitHubIcon>
@@ -33,7 +40,7 @@ export default function About() {
                 <Button
                     href="www.linkedin.com/in/gabriela-torrini-9a2ab7235" 
                     variant="contained" 
-                    sx={{mx: 1}}
+                    sx={{mx: 1, mb: (window.innerWidth < 715) ? 1 : 0}}
                 >
                     Connect with Me &nbsp;
                     <LinkedInIcon></LinkedInIcon>
