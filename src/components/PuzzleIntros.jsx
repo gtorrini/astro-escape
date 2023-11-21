ab74c79366c311636c982f982e08d3bd6a26a022
Gabriela Torrini <gabriela.torrini@outlook.com>
2023-11-20 20:56:05 -0600

    Added responsive styling & optional children prop

--- src/components/PuzzleIntros.jsx
+++ src/components/PuzzleIntros.jsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import { React, useContext } from 'react';
 import PropTypes from 'prop-types';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 
+import { ViewportContext } from './useViewport.js';
+
 export default function PuzzleIntro(props) {
+  const width = useContext(ViewportContext);
+
+  const checkChildren = () => {
+    if (props.children) {
+      return (
+        props.children
+      );
+    }
+  }
+
   return (
     <Box 
       sx={{ 
-        maxWidth: (window.innerWidth <= 650) ? '100%' : '60%',
+        maxWidth: (width <= 650) ? '95%' : '60%',
         mx: 'auto',
         my: 5 
       }}
@@ -15,14 +27,19 @@ export default function PuzzleIntro(props) {
       <Typography className="site-heading" variant='h5' gutterBottom>
         {props.title}
       </Typography>
-      <Typography variant='body1' sx={{ textAlign: 'center' }}>
+      <Typography 
+        variant='body1' 
+        sx={{ mb: (props.children) ? 3 : 0, textAlign: 'center' }}
+      >
         {props.blurb}
       </Typography>
+      {checkChildren()}
     </Box>
   );
 }
 
 PuzzleIntro.propTypes = {
   title: PropTypes.string.isRequired,
-  blurb: PropTypes.string.isRequired
+  blurb: PropTypes.string.isRequired,
+  children: PropTypes.element
 }
