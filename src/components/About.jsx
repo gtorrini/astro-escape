e956675da39eb136f6c8e3d38e8a14e064a180b1
Gabriela Torrini <gabriela.torrini@outlook.com>
2023-11-20 20:56:38 -0600

    Refactored to use PuzzleIntros component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,53 +1,53 @@
+// 3rd-party imports
 import { React } from 'react';
-import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
-import Typography from '@mui/material/Button';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LanguageIcon from '@mui/icons-material/Language';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
+// Local imports
+import PuzzleIntros from './PuzzleIntros.jsx';
+
+function Networking() {
+  return (
+    <>
+      <Button 
+        href="https://gtorrini.github.io/"
+        variant="contained"
+        sx={{ mx: 1, mb: 1 }}
+      >
+        Visit Personal Site &nbsp;
+        <LanguageIcon></LanguageIcon>
+      </Button>
+      <Button 
+        href="https://github.com/gtorrini/portfolio"
+        variant="contained"
+        sx={{ mx: 1, mb: 1 }}
+      >
+        View Portfolio &nbsp;
+        <GitHubIcon></GitHubIcon>
+      </Button>
+      <Button
+        href="www.linkedin.com/in/gabriela-torrini-9a2ab7235" 
+        variant="contained" 
+        sx={{ mx: 1, mb: 1 }}
+      >
+        Connect with Me &nbsp;
+        <LinkedInIcon></LinkedInIcon>
+      </Button>
+    </>
+  );
+}
+
 export default function About() {
-    return (
-        <>
-            <Box sx={{my: 5}}>
-                <Typography className="site-heading" variant="h4">
-                    ABOUT
-                </Typography>
-                <Typography 
-                  variant="body1" 
-                  sx= {{
-                    my: 3, 
-                    maxWidth: (window.innerWidth < 480) ? '90%' : '60%', 
-                    mx: 'auto'
-                  }}
-                >
-                    Astro Escape was developed by Gabriela Torrini, a software engineer and astronomer. She also designed the game&#39;s pixel art background. 
-                </Typography>
-                <Button 
-                    href="https://gtorrini.github.io/"
-                    variant="contained"
-                    sx={{ mx: 1, mb: (window.innerWidth < 715) ? 1 : 0}}
-                >
-                    Visit Personal Site &nbsp;
-                    <LanguageIcon></LanguageIcon>
-                </Button>
-                <Button 
-                    href="https://github.com/gtorrini/portfolio"
-                    variant="contained"
-                    sx={{mx: 1, mb: (window.innerWidth < 715) ? 1 : 0}}
-                >
-                    View Portfolio &nbsp;
-                    <GitHubIcon></GitHubIcon>
-                </Button>
-                <Button
-                    href="www.linkedin.com/in/gabriela-torrini-9a2ab7235" 
-                    variant="contained" 
-                    sx={{mx: 1, mb: (window.innerWidth < 715) ? 1 : 0}}
-                >
-                    Connect with Me &nbsp;
-                    <LinkedInIcon></LinkedInIcon>
-                </Button>
-            </Box>
-        </>
-    );
+  const heading = 'ABOUT'
+  const bio = "Astro Escape was developed by Gabriela Torrini, a software engineer and astronomer. She also designed the game's pixel art background.";
+
+  return (
+    <>
+      <PuzzleIntros title={heading} blurb={bio}>
+        <Networking/>
+      </PuzzleIntros>
+    </>
+  );
 }
