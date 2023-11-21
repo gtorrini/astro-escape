3842363b9126342d443d80a40e80600622babd5b
Gabriela Torrini <gabriela.torrini@outlook.com>
2023-11-20 20:57:11 -0600

    Fixed file formatting

--- src/components/Outro.jsx
+++ src/components/Outro.jsx
@@ -4,9 +4,10 @@ import PuzzleIntros from './PuzzleIntros.jsx';
 export default function Outro() {
   const heading = '\u2726 CONGRATULATIONS \u2726';
   const closing = 'Thanks for playing!';
-    return (
-        <>
-          <PuzzleIntros title={heading} blurb={closing} />
-        </>
-    );
+
+  return (
+    <>
+      <PuzzleIntros title={heading} blurb={closing} />
+    </>
+  );
 }
